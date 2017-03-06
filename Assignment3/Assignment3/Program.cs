513c8e2118581bbb03a835d68ccc512c34e23b22
geobi <geobi>
2017-03-06 12:11:10 +0100

    Assignment3

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questionaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. What are the colors of the Nigerian flag?");
+            string input = Console.ReadLine();
+            Console.WriteLine("Print Out: " + input);
+            Console.WriteLine("2. Where is the capital of Akwa Ibom State?");
+            input = Console.ReadLine();
+            Console.WriteLine("Print Out: " + input);
+            Console.WriteLine("3. Who is the President of Nigeria?");
+            input = Console.ReadLine();
+            Console.WriteLine("Print Out: " + input);
+            Console.WriteLine("4. In what year did the founder of Facebook come to Nigeria?");
+            input = Console.ReadLine();
+            Console.WriteLine("Print Out: " + input);
+            Console.WriteLine("5. What is the full meaning of HTML?");
+            input = Console.ReadLine();
+            Console.WriteLine("Print Out: " + input);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
