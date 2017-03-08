64f840df1c0a04ef36c6a094e9df24c66f08268a
geobi <geobi>
2017-03-08 15:18:17 +0100

    Country template

---
+++ CountryTemplate/CountryTemplate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+           NewCountry.Name = "Cameroun";
+           NewCountry.GeoLandMass = 475442;
+           NewCountry.CollectionOfStates = 10;
+           NewCountry.Population = 22250000;
+           NewCountry.GDP = 29.570,000,000;
+           NewCountry.President = "Paul Biya";
+
+        }
+    }
+    class NewCountry
+    {
+         
+        public string Name { get; set; }
+        public int GeoLandMass { get; set; }
+        public enum CollectionOfStates { get; set; }
+        public long Population { get; set; }
+        public long GDP { get; set; } 
+        public string President { get; set; }
+
+        public static char AddState (char Population, int landmass, int LGA);
+    {
+        return 
+    }
+ }
+}
